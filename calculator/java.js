44f976c11f9533c09262b5a9f695f95bc61f6e31
HoangThien <17T1021253@husc.edu.vn>
2021-03-17 17:03:30 +0700

    up

--- calculator/java.js
+++ calculator/java.js
@@ -1,4 +1,7 @@
 var str="";
+var operator=false;
+var toEpt=false;
+
 var input=document.querySelector('.screen');
 var items=Array.from(document.querySelectorAll('.button'));
 items.forEach(function(btn){
@@ -7,24 +10,38 @@ items.forEach(function(btn){
             input.innerHTML='0';
             str='0';
         }
-        
         if(btn.innerHTML=='AC'){
             input.innerHTML='0';
+            str="0";
+            operator=false;
+            toEpt=false;
             
         }
-        else if(btn.innerHTML=='%'){
-            input.innerHTML=input.innerHTML+btn.innerHTML;
-            str+='/100';
-            str=eval(str);
-            input.innerHTML=str;
-        }
-        
         else if(btn.innerHTML=='='){
             if(str.charAt(str.length-1)=='+'||str.charAt(str.length-1)=='-'||str.charAt(str.length-1)=='*'||str.charAt(str.length-1)=='/')
                 str = str.substring(0, str.length - 1);
             str=eval(str);
+            str+='';
+            if(str.length>8){
+                var a= parseFloat(str).toExponential(5);
+                str=a.toString();
+                toEpt=true;
+            }          
             input.innerHTML=str;
             str+='';
+            operator=false;
+        }
+        else if(str.length<13){
+        if(btn.innerHTML=='%'){
+            input.innerHTML=input.innerHTML+btn.innerHTML;
+            str+='/100';
+            str=eval(str);
+            str+='';
+            if(str.length>8){
+                var a= parseFloat(str).toExponential(5);
+                str=a.toString();
+            } 
+            input.innerHTML=str;
         }
         else if(btn.innerHTML=='+/-'){
             input.innerHTML=(input.innerHTML)*(-1);
@@ -34,40 +51,89 @@ items.forEach(function(btn){
             if(str.charAt(str.length-1)=='+'||str.charAt(str.length-1)=='-'||str.charAt(str.length-1)=='*'||str.charAt(str.length-1)=='/')
                 str=str.substring(0, str.length - 1);
             str=eval(str);
+            str+='';
+            if(str.length>8){
+                var a= parseFloat(str).toExponential(5);
+                str=a.toString();
+            } 
             input.innerHTML=str+'x';
             str=str+'*';
+            operator=true;
         }
         else if(btn.innerHTML=='÷'){
             if(str.charAt(str.length-1)=='+'||str.charAt(str.length-1)=='-'||str.charAt(str.length-1)=='*'||str.charAt(str.length-1)=='/')
                 str = str.substring(0, str.length - 1);
         
             str=eval(str);
+            str+='';
+            if(str.length>8){
+                var a= parseFloat(str).toExponential(5);
+                str=a.toString();
+            } 
             input.innerHTML=str+'÷';
             str+='/';
+            operator=true;
         }
         else if(btn.innerHTML=='-'){
             if(str.charAt(str.length-1)=='+'||str.charAt(str.length-1)=='-'||str.charAt(str.length-1)=='*'||str.charAt(str.length-1)=='/')
                 str=str.substring(0, str.length -1);
             str=eval(str);
+            str+='';
+            if(str.length>8){
+                var a= parseFloat(str).toExponential(5);
+                str=a.toString();
+            } 
             input.innerHTML=str+'-';
             str+='-';
+            operator=true;
         }
         else if(btn.innerHTML=='+'){
             if(str.charAt(str.length-1)=='+'||str.charAt(str.length-1)=='-'||str.charAt(str.length-1)=='*'||str.charAt(str.length-1)=='/')
                 str=str.substring(0, str.length - 1);
             str=eval(str);
+            str+='';
+            if(str.length>8){
+                var a= parseFloat(str).toExponential(5);
+                str=a.toString();
+            } 
             input.innerHTML=str+'+';
             str+='+';
+            operator=true;
         }
         else if(btn.innerHTML==','){
             input.innerHTML=(input.innerHTML)+'.';
             str=str+'.';
         }
-        else {
+        else if(btn.innerHTML!="AC" && btn.innerHTML!='=')
+         {
             if(input.innerHTML=='0')
                 input.innerHTML='';
-            input.innerHTML+=btn.innerHTML;
-            str=str+btn.innerHTML;
+            if( (str.length>9) && (operator ==false) ){
+                var b= parseFloat(btn.innerHTML);
+                if(toEpt==true){
+                    var a = (parseFloat(str)*10+b);
+                    a=a.toExponential(5);
+                    str = a.toString();
+                }
+                else{
+                    str=str+btn.innerHTML;
+                    var a= parseFloat(str).toExponential(5);
+                    toEpt=true;
+                    str=a.toString();
+                }
+                input.innerHTML=str;
+            }
+            else{
+                input.innerHTML+=btn.innerHTML;
+                str=str+btn.innerHTML;
+            }
+
+                
+            
         }
+        }
+        else
+            alert("Full screen")
+       
     })
 })
